8f85cbbd45cce1b223ef9d9224848bc38ff7019a
alpfit <alpfit@gmail.com>
2023-05-22 18:48:52 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,8 @@
+function skillMembers() {
+  var members = [
+    { name: 'John', age: 23, skills: ['Javascript', 'CSS', 'HTML'] },
+    { name: 'Jane', age: 20, skills: ['Javascript', 'CSS', 'HTML'] },
+    { name: 'Jack', age: 20, skills: ['Javascript', 'CSS', 'HTML'] },
+    { name: 'Jill', age: 20, skills: ['Javascript', 'CSS', 'HTML'] },
+  ];
+}   
